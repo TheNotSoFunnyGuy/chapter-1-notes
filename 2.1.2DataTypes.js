65230ac93d1a1d3f13e3ff2a02fb341f997d36fe
uquo <uquo@fresnou.org>
2023-02-06 10:07:32 -0800

    initial commit

---
+++ 2.1.2DataTypes.js
@@ -0,0 +1,41 @@
+console.log(7);
+//This is a comment
+
+//Numbers
+//Integers
+console.log(-3);
+console.log(0);
+console.log(3);
+//Floating Point Number
+console.log(3.14);
+console.log(100.045);
+
+//Strings
+//Characters, letters, symbols, spaces
+console.log("hello world");
+console.log('hello world!!');
+console.log(`hello world 123`);
+console.log('16.0');
+
+//Arithmetic Operators (Math)
+console.log(14+2);
+console.log(14-2);
+console.log(14*2);  //shift-8 gives us *
+console.log(14/2);
+console.log(14%2); //% is called MOD and means remainder after dividing
+console.log(15%10);
+
+//Concatenation: combine strings
+console.log("My name"+" is Alexes");
+console.log('My' + ' ' + 'age' + ' '+'is'+ ' '+ 15 + '.');
+console.log("Your age is\n17.");
+
+//Template literal - plug in calculated value
+//use backticks `` next to the 1
+console.log(`15 devided by 10 = ${15/10} axcvzxcvx`);
+
+
+
+console.log(`200 devided by 8 equals ${200/8}`);
+
+console.log(21 != 20 ? "YOU WIN!" : "YOU LOSE!");
---
+++ operators.js
@@ -0,0 +1,25 @@
+//Unary Operators use only one value
+console.log(typeof 15);
+console.log(typeof "this string");
+console.log(typeof true);
+console.log(typeof false);
+
+//Binary Operators use two values
+//Comparison Operators
+console.log(10 > 5);  //greater than
+console.log(10 < 5); //less than
+console.log(10 >= 5); //greater than or equal to
+console.log(10 <= 5); //less than or equal to
+console.log(10 == 5); //are they both equal? false
+console.log(10 == 10); //are they both equal? true
+console.log(10 != 5); //are they different? true
+console.log(10 != 10); //are they different? false
+//Logical Operators
+
+
+//Ternary Operators use three values
+
+
+
+
+console.log("Hello" + "\nWorld")
